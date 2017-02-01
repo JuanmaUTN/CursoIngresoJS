e9be686e9bd4f3873f986285b0272362604a100e
JuanmaUTN <juanmacostas99@live.com>
2017-02-01 12:00:06 -0300

    Add files via upload
    1/02/2017 PRIMER CLASE DE PROGRAMACIÒN

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,17 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
+	/*Esto es un comentario de varias lìneas
+	sadsadasdsadasdasdsadasdsadasdasdasdasd*/
 	
+	//Esto es un comentario de una lìnea
+
+	var person = prompt("Ingresa tu nombre", "Natalia Natalia"); //ESTA LÌNEA NO ES NECESARIA
+	
+	var nombre; //Defino variable
+	nombre = "Juan Manuel"; //Cargo variable (ESTA LÌNEA NO ES NECESARIA)
+	nombre = prompt("Ingrese su nombre", "Natalia Natalia");
+	alert("Nombre: "+nombre); //Valores concatenales
+
 }
 
