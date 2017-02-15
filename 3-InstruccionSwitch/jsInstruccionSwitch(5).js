99d06aada365371132a8fa2d2ccc8bbbffbc087e
JuanmaUTN <juanmacostas99@live.com>
2017-02-15 07:15:14 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -5,11 +5,11 @@ var laHora = document.getElementById('hora').value;
 
 switch(laHora)
 	{
-		case 7:
-		case 8:
-		case 9:
-		case 10:
-		case 11:
+		case "7":
+		case "8":
+		case "9":
+		case "10":
+		case "11":
 		alert("Es de mañana.");
 		break;
 
